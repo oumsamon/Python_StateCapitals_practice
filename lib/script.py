64fe82c27c314b1570d309e2b3ba9cced3368383
ls <cancel>
2021-09-14 20:14:57 -0500

    welcome message input()

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,12 @@
 from capitals import states
 
-print(states)
+# print(states)
+
+
+print("Welcome to match the State to the capitals!  Would you like to play?  Yes or No")
+game = input()
+
+if game == "Yes" or "yes":
+    print(f"Welcome to game of matching the State to the Capitals!!")
+else:
+    print("Hope to see you again soon!")
