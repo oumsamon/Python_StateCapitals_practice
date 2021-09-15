b96163a718b0eac3d873f7693f3826f805ebe03d
ls <cancel>
2021-09-14 20:20:28 -0500

    update to accept Yes and yes

--- lib/script.py
+++ lib/script.py
@@ -6,7 +6,7 @@ from capitals import states
 print("Welcome to match the State to the capitals!  Would you like to play?  Yes or No")
 game = input()
 
-if game == "Yes" or "yes":
+if game.lower() == "yes":
     print(f"Welcome to game of matching the State to the Capitals!!")
 else:
     print("Hope to see you again soon!")
